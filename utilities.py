92c729f65aaf77424fe9e49b584d66a6e7b0ad13
ConsoleRunner <wojciechly@wp.pl>
2021-04-17 05:08:35 +0200

    feat: rudimentary documentation in FATProxy

--- utilities.py
+++ utilities.py
@@ -1,24 +1,36 @@
 from bidict import bidict
 
 class FATProxy:
-    def __init__(self, fat_byte_offset, fat_byte_size, io):
+    def __init__(self, fat_byte_offset, fat_byte_size, bytes_per_ls, io):
         self._io = io
         self._fat_offset = fat_byte_offset
         self._fat_size = fat_byte_size
+        self._bytes_per_ls = bytes_per_ls
         self._read()
 
     def _read(self):
-        # parse whole FAT - generate bidicr
+        # Each FAT record is 32 bits
+        # bits 0-6:  record_nr relative to FAT sector (128 per sector)
+        # bits 7-31: ls_nr, sector number relative to FAT beginning
+        _pos = self._io.pos()
+        aux = {}
 
-        # fat_record_byte_offset = cluster_nr * 32 # ???
+        # cluster_nr = ls_nr * 128 + record_nr
+        # byte_offset = self._fat_offset + cluster_nr * 32
+        # byte_offset = self._fat_offset + ls_nr * self._bytes_per_ls + record_nr * 32
 
-        # _pos = self._io.pos()
-        # self._io.seek(fat_record_byte_offset)
+        # Only parsing dangling records, for efficiency sake
+        records_left = set()    # TODO: add first cluster nr (should be 2 - check)
+        while records_left:
+            # self._io.seek(fat_record_byte_offset)
+            # aux = self._io.read_u4le()
+            # TODO: ignore values considered as BAD CLUSTER (0x0FFFFFF7),
+            #       and END CLUSTER (>=0x0FFFFFF8)
+            # TODO: when seeking check if not moving out of fat_size, just in case
+            pass
 
-        # aux = self._io.read_u4le()
-        # self._io.seek(_pos)
-
-        self._fat = bidict({123: 456})
+        self._fat = bidict(aux)
+        self._io.seek(_pos)
 
     def get_next_cluster(self, cluster_nr):
         try:
