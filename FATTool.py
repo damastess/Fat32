1c07ee7439bcb28e9a21d92820ee5c5f33246f8f
Kovalio <kowal.bartek.10@gmail.com>
2021-04-26 11:36:08 +0200

    First test version of tool

--- FATTool.py
+++ FATTool.py
@@ -1,6 +1,6 @@
 from kaitai_generated.mbr_partition_table import MbrPartitionTable
 from kaitai_generated.vfat import Vfat
-from utilities import FATProxy
+from utilities import FATProxy, Filesystem
 import argparse
 
 
@@ -22,16 +22,51 @@ def setup():
 
 if __name__ == '__main__':
     args = setup()
-    mbr_data = MbrPartitionTable.from_file(args.filename)
-
+    print("===================================================================================")
+    print("Arguments: ")
     print(args.filename, args.mbr_sector_size, args.sector_nr)
+    mbr_data = MbrPartitionTable.from_file(args.filename)
+    MBR_SECTOR_SIZE = args.mbr_sector_size
+    START_SECTOR = args.sector_nr
 
-    for partition in mbr_data.partitions:
+    last_part_start = 0
+    main_partition = mbr_data.partitions[0]
+    for id, partition in enumerate(mbr_data.partitions):
         if partition.lba_start != 0:
-            io = mbr_data._io
-            io.seek(args.mbr_sector_size * partition.lba_start)
-            vfat_partition = Vfat(io)
+            if START_SECTOR < partition.lba_start:
+                main_partition = mbr_data.partitions[id]
+            last_part_start = partition.lba_start
 
-            fat_proxy = FATProxy(vfat_partition.fats(args.mbr_sector_size * partition.lba_start).records)
-            print(fat_proxy._fat)
-            print(vfat_partition.file().file_records)
+    partition_offset = MBR_SECTOR_SIZE * main_partition.lba_start
+    io = mbr_data._io
+    io.seek(partition_offset)
+    vfat_partition = Vfat(io)
+    fat_proxy = FATProxy(vfat_partition.fats(partition_offset).records)
+    filesystem_offset = partition_offset + vfat_partition.boot_sector.pos_root_dir
+    bytes_per_cluster = vfat_partition.boot_sector.bpb.bytes_per_ls * vfat_partition.boot_sector.bpb.ls_per_clus
+    files = Filesystem(fat_proxy, filesystem_offset, bytes_per_cluster, io)
+
+    start_sector_in_bytes = START_SECTOR * MBR_SECTOR_SIZE
+    searching_cluster = -1
+    sectorpercluster = vfat_partition.boot_sector.bpb.ls_per_clus
+    bytespersector = vfat_partition.boot_sector.bpb.bytes_per_ls
+    clusters_in_partition = main_partition.num_sectors // 8
+    i = 0
+    while i < clusters_in_partition:
+        i += 1
+        if filesystem_offset + sectorpercluster * bytespersector * i > start_sector_in_bytes:
+            searching_cluster = 1 + i
+            break
+
+    if searching_cluster == -1:
+        print("Error, couldn't find cluster for picked sector")
+    else:
+        start_cluster_of_searching_file = fat_proxy.get_first_cluster(searching_cluster)
+
+        for file in files._files_list:
+            if not file.long_filename:
+                if file.start_file_in_cluster == start_cluster_of_searching_file:
+                    print('==================')
+                    for name, value in file.__dict__.items():
+                        print(name, value)
+                    break
